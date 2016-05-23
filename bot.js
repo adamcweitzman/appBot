4654cccd1e3224a054fafba252b51e8c15715249
adamcweitzman <adamcweitzman@gmail.com>
2016-05-23 18:34:50 -0500

    add loop to go through swiping until likes run out

--- bot.js
+++ bot.js
@@ -1,20 +1,39 @@
 var tinder = require('tinder');
 var client = new tinder.TinderClient();
+var _ = require('underscore')
 
-client.authorize(
-  "EAAGm0PX4ZCpsBAJ1pZBLgBF55Mv2ndYU5cqptrbr7pltUIlihXqvSLeyVnyJEjH9tzo5XgOXDQnTXTYkZBsRryyQB4aKSjBh3ZAzNEpGOEa0p2De86ZBZAJhG1JN2CS6Eyhe41xi1gk7uk0kZC5a36jAZBChX6900wckkZAnQEu4ZAHwZDZD",
-  10152982194278478,
-  function() {
-  	var isAuth = client.isAuthorized();
-  	console.log(isAuth)
-    client.getRecommendations(10, function(error, data){
-      if(data == null) {
-      	console.log("bad auth")
-      }
-      else {
-      	console.log("light my fire", data.results)
-      }
-  });
+client.authorize("EAAGm0PX4ZCpsBAJ1pZBLgBF55Mv2ndYU5cqptrbr7pltUIlihXqvSLeyVnyJEjH9tzo5XgOXDQnTXTYkZBsRryyQB4aKSjBh3ZAzNEpGOEa0p2De86ZBZAJhG1JN2CS6Eyhe41xi1gk7uk0kZC5a36jAZBChX6900wckkZAnQEu4ZAHwZDZD", 10152982194278478, function() {
+
+    var defaults = client.getDefaults()
+    var recs_size = defaults.globals.recs_size;
+
+    var isAuth = client.isAuthorized();
+    console.log(isAuth);
+
+    client.getRecommendations(recs_size, function(error, data){
+    	console.log(data);
+    	count = 0;
+    	for (i = 0; i < 20; i++) { 	
+	      _.chain(data.results)
+	        .pluck('_id')
+	        .each(function(id) {
+	          client.like(id, function(error, data) {
+	          	count++
+	          	console.log("this is person number " + count, "and their id is " + id)
+	          	console.log(count)
+	          	console.log(data)
+	            if (data == null) {
+	              console.log("no data for ",id);
+	            } else if (data.match == false) {
+	              console.log("not a match with " + id);		
+	            } else if (data.match) {
+	              client.sendMessage(id, "hey little troublemaker ;)");
+	              	console.log("message sent to " + id);
+	            }
+	          });
+	        });
+	    }
+    });
 });
 
 //fbid 10154237716728478
